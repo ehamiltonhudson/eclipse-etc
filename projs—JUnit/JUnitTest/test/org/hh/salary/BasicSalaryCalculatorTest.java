150a27aec7a736511d0998773b718a35abf25665
Hamilton <ehamiltonhudson@gmail.com>
2019-10-13 19:44:28 -0400

    added to reorg - salary/BasicSalaryCalculatorTest.java

--- projs—JUnit/JUnitTest/test/org/hh/salary/BasicSalaryCalculatorTest.java
+++ projs—JUnit/JUnitTest/test/org/hh/salary/BasicSalaryCalculatorTest.java
@@ -1,24 +1,50 @@
 package org.hh.salary;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import org.hh.salary.BasicSalaryCalculator;
+
 
 class BasicSalaryCalculatorTest {
 
+	private BasicSalaryCalculator basicSalaryCalculator;
+	
 	@BeforeEach
-	void setUp() throws Exception {
+	void init() {
+		basicSalaryCalculator = new BasicSalaryCalculator();
 	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testBasicSalaryWithValidSalary() {
+		double basicSalary = 4000;
+		basicSalaryCalculator.setBasicSalary(basicSalary);
+		
+		double expectedSocialInsurance = basicSalary * 0.25;
+		assertEquals(expectedSocialInsurance, basicSalaryCalculator.getSocialInsurance());
+		
+		double expectedAdditionalBonus = basicSalary * 0.1;
+		assertEquals(expectedAdditionalBonus, basicSalaryCalculator.getAdditionalBonus());
+		
+		double expectedGross = basicSalary + expectedSocialInsurance + expectedAdditionalBonus;
+		assertEquals(expectedGross, basicSalaryCalculator.getGrossSalary());
+	}
+	
+	@DisplayName("Test BasicSalaryCalculator with invalid salary")
+	@Test
+	void testBasicSalaryWithInvalidSalary() {
+		double basicSalary = -100;
+		assertThrows(IllegalArgumentException.class, () -> {
+			basicSalaryCalculator.setBasicSalary(basicSalary);
+		});
+	}
+	
+	@AfterEach
+	void tearDown() {
+		basicSalaryCalculator = null;
 	}
 
 }
